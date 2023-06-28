af7d3fb77364d75d275e7642b1aeea4d62bc2f0f
Anatoly Karpov <karpik-ne@yandex.ru>
2023-06-28 10:19:21 +0700

    1

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,9 +1,6 @@
 const jwt = require('jsonwebtoken');
 
 const AuthError = require('../errors/autherror');
-const {
-  STATUS_OK,
-} = require('../utils/constants');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
@@ -18,8 +15,6 @@ module.exports = (req, res, next) => {
 
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
-    res.status(STATUS_OK).send({ message: 'Успешная авторизация' });
-    // .send({ token })
   } catch (err) {
     next(new AuthError('Необходимо авторизоваться'));
     return;
