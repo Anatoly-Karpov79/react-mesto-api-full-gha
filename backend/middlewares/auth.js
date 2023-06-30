e1ce3b32e21cd73b337b90dc9559332e7d4a13e9
Anatoly Karpov <karpik-ne@yandex.ru>
2023-07-01 00:00:29 +0700

    1

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,20 +1,21 @@
-const jwt = require('jsonwebtoken');
+const token = require('jsonwebtoken');
 
 const AuthError = require('../errors/autherror');
 
 const { JWT_SECRET = 'dev-secret' } = process.env;
 
 module.exports = (req, res, next) => {
-  if (!req.cookies) {
+  const { jwt } = req.cookies;
+  if (!jwt) {
     next(new AuthError('Необходимо срочно авторизоваться'));
     return;
   }
-  const token = req.cookies.jwt;
+ // const token = req.cookies.jwt;
 
   let payload;
 
   try {
-    payload = jwt.verify(token, JWT_SECRET);
+    payload = token.verify(jwt, JWT_SECRET);
   } catch (err) {
     next(new AuthError('Необходимо авторизоваться'));
     return;
