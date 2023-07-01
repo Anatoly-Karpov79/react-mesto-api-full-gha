d8929bf4cbddb175e33eb999629b80ff586a2294
Anatoly Karpov <karpik-ne@yandex.ru>
2023-07-01 19:48:05 +0700

    1

--- backend/app.js
+++ backend/app.js
@@ -5,18 +5,18 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
+const cors = require('./middlewares/cors');
 const routerCards = require('./routes/cards');
 const routerUsers = require('./routes/users');
 const NotFoundError = require('./errors/notfounderror');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const cors = require('./middlewares/cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors);
 // ({ origin: ['http://localhost:3000'], credentials: true })
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
