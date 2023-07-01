dad9b076b61197639a3ec6c39a02dd5a983c5ec5
Anatoly Karpov <karpik-ne@yandex.ru>
2023-07-01 19:05:32 +0700

    1

--- backend/app.js
+++ backend/app.js
@@ -16,8 +16,8 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(cors({ origin: ['http://localhost:3000'], credentials: true }));
-
+app.use(cors);
+// ({ origin: ['http://localhost:3000'], credentials: true })
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 });
