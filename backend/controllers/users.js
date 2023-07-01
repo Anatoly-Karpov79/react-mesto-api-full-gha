6dc86f82431085586a6bc8a7b73645df11374417
Anatoly Karpov <karpik-ne@yandex.ru>
2023-07-01 21:55:57 +0700

    1

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -119,7 +119,7 @@ module.exports.login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, 'super-strong-secret', { expiresIn: '7d' });
-      res.status(STATUS_OK).cookie('userId', token, {
+      res.status(STATUS_OK).cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
       }).send({ message: 'Успешная авторизация' });
