a51c55419a6526e57d8cb4ba03bae6bf1bc17cd2
Anatoly Karpov <karpik-ne@yandex.ru>
2023-07-02 22:16:07 +0700

    - samSite

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -124,8 +124,8 @@ module.exports.login = (req, res, next) => {
       res.status(STATUS_OK).cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
-        sameSite: 'None',
-        secure: true,
+      //  sameSite: 'None',
+      // secure: true,
       }).send(user.toJSON());
     })
     .catch(next);
