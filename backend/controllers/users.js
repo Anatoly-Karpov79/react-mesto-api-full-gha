09c5d5c288f1335605c5a4b060fa0bab663ff898
Anatoly Karpov <karpik-ne@yandex.ru>
2023-07-02 12:53:29 +0700

    1

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -124,7 +124,7 @@ module.exports.login = (req, res, next) => {
         httpOnly: true,
         sameSite: 'None',
         secure: true,
-      }).send({ token });
+      }).send(user);
     })
     .catch(next);
 };
