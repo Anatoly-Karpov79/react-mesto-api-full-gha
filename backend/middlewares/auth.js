579047a7aea9ee15a9875528652fbdfe3dda6c9e
Anatoli Karpov <karpik-ne@yandex.ru>
2024-12-25 12:09:06 +0100

    merge

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -5,14 +5,22 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 const AuthError = require('../errors/autherror');
 
 module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
+  const { authorization } = req.headers;
+//  const token 
+   if (!authorization || !authorization.startsWith('Bearer ')) {
+    return res
+      .status(401)
+      .send({ message: 'Необходима авторизация hhhh' });
+   }
 
+  const token = authorization.replace('Bearer ', '');
+  
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    next(new AuthError('Необходимо авторизоваться token'));
+    next(new AuthError('Необходимо авторизоваться token 121'));
     return;
   }
 
